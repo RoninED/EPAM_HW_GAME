a26106b65617608a0195bd28d126142ffa86c9b9
Eduard_Gimadiev <Nameeduard@yandex.ru>
2022-05-07 00:05:29 +0300

    method tryToUseAbility

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,33 +1,59 @@
+import abilities.Abilities;
 import creatures.utils.AbstractCreature;
 import creatures.utils.DataOfCreatures;
 import gamePlay.GameMap;
 
 public class Main {
+    static AbstractCreature player1;
+    static AbstractCreature player2;
+
     public static void main(String[] args) {
         gamePlay();
     }
 
-    static void gamePlay(){
+    static void gamePlay() {
         phase1Creating();
+        actionChooser(player1, player2);
     }
 
-    private static void phase1Creating(){
+    private static void phase1Creating() {
         System.out.println("Player 1, choose you fighter!!!");
-        AbstractCreature Player1Creature = DataOfCreatures.heroFactory();
-        GameMap.setCreatureToCell(Player1Creature, 1);
+        player1 = DataOfCreatures.heroFactory();
+        GameMap.setCreatureToCell(player1, 1);
 
-        System.out.println(Player1Creature.ID);
+
         System.out.println("Player 2, choose you fighter!!!");
-        AbstractCreature Player2Creature = DataOfCreatures.heroFactory();
-        GameMap.setCreatureToCell(Player2Creature, 10);
+
+        player2 = DataOfCreatures.heroFactory();
+        GameMap.setCreatureToCell(player2, 2);
 
         DataOfCreatures.printListOfCreatures();
 
         GameMap.printMap1();
+
     }
 
-    void actionChooser(){
-        
+    private static void actionChooser(AbstractCreature player, AbstractCreature enemy) {
+        boolean actionIsDone = false;
+
+        actionIsDone = tryToUseAbility(player, enemy);
+    }
+
+    private static boolean tryToUseAbility(AbstractCreature player, AbstractCreature enemy){
+        boolean actionIsDone = false;
+
+        for (Abilities ability : player.ABILITIES
+        ) {
+            if (ability.isItUsable(player, enemy)){
+                if (ability.getIsItForSelf()){
+                    ability.use(player, player);
+                }
+                else ability.use(player, enemy);
+                actionIsDone = true;
+                break;
+            }
+        }
+        return actionIsDone;
     }
 
 
